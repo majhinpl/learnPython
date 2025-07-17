354747f88389fc05ca1042a81049b1c52d5659fe
nepal majhi <nepal.majhi@bfim.com.sa>
2025-07-17 09:18:48 +0300

    add python in shell

---
+++ 10-python-in-shell/Adjusted_price_calculator.py
@@ -0,0 +1,12 @@
+def greet(your_name):
+    print("Namaste!", your_name)
+
+exit_shares = 1254624521
+cmp = 239
+
+new_shares = 1254624521
+shares_vpu = 157
+
+adj_price = exit_shares + cmp 
+print(adj_price)
+greet("nephx")
